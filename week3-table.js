d83ad4c540ee0bd850a1d06cfe14fab63f1424c7
shamiul <shoq9876@gmail.com>
2016-06-10 23:43:07 +0600

    week 3 task added

---
+++ week3-table.js
@@ -0,0 +1,74 @@
+let attendanceObject = {
+  startDate: "june 7, 2014",
+  endDateLength: 7,
+  attendanceArray: [
+    {
+      name: "Ted",
+      aList: [
+        true,
+        false,
+        true,
+        true,
+        true,
+        false,
+        true
+      ]
+    }, {
+      name: "John",
+      aList: [
+        false,
+        false,
+        true,
+        true,
+        true,
+        false,
+        true
+      ]
+    }, {
+      name: "Dave",
+      aList: [
+        false,
+        false,
+        false,
+        true,
+        true,
+        false,
+        true
+      ]
+    }, {
+      name: "Tom",
+      aList: [
+        true,
+        true,
+        true,
+        true,
+        true,
+        true,
+        true
+      ]
+    }, {
+      name: "Mat",
+      aList: [
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false
+      ]
+    }, {
+      name: "Gregory",
+      aList: [
+        true,
+        false,
+        false,
+        false,
+        false,
+        false,
+        true
+      ]
+    }
+
+  ]
+};
