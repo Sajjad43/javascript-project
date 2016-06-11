59ea44704d57deb5cc439a99df72317d22e70ef2
Sajjad Hossen <hmohamedsajjad43@gmail.com>
2016-06-11 13:29:01 +0600

    week3 task uploaded

--- week3-table.js
+++ week3-table.js
@@ -1,3 +1,10 @@
+
+document.write("<h1>Hello</h1>");
+
+
+
+
+
 let attendanceObject = {
   startDate: "june 7, 2014",
   endDateLength: 7,
@@ -68,7 +75,63 @@ let attendanceObject = {
         false,
         true
       ]
+    },
+    {
+      name: "klll",
+      aList: [
+        true,
+        false,
+        false,
+        false,
+        false,
+        false,
+        true
+      ]
     }
 
   ]
 };
+
+document.write("<table class='table table-bordered'>");
+      var date=new Date(attendanceObject.startDate);
+      /*document.write("<h1>"+ date.getDate() +"</h1>");*/
+      var d = date.getDate();
+      var m = date.getMonth() + 1;
+      var y = date.getFullYear();
+
+
+                  document.write("<tr> <td>"+'name'+"</td>");
+                    for(var k = 0; k < attendanceObject.endDateLength; k++)
+                    {
+                      document.write("<td >" +(d+k) +"/" +m +"/" +y  +"</td>");
+                    }
+                  document.write("</tr>");
+
+                  for(var i=0;i<attendanceObject.attendanceArray.length;i++)
+                  {
+                      document.write("<tr><td >"+ attendanceObject.attendanceArray[i].name +"</td>");
+
+                        for(var j=0;j<attendanceObject.attendanceArray[i].aList.length;j++){
+                          document.write("<td style='background-color:blue' onClick='myFunction(this)' id='"+i+"-"+j+ "'>"+attendanceObject.attendanceArray[i].aList[j]+"</td>");
+
+                        }
+
+
+                        document.write("</tr>");
+                  }
+
+document.write("</table>");
+var myFunction=function (event) {
+   console.log(event);
+   var split=event.id.split("-");
+   var i=parseInt(split[0],10);
+   var j=parseInt(split[1],10);
+
+   console.log(attendanceObject.attendanceArray[i].aList[j]);
+   attendanceObject.attendanceArray[i].aList[j]=!attendanceObject.attendanceArray[i].aList[j];
+   event.style.backgroundColor="rgb(255, 255, 255)";
+   event.innerHTML=""+attendanceObject.attendanceArray[i].aList[j];
+   console.log(attendanceObject.attendanceArray[i].aList[j]);
+
+
+}
